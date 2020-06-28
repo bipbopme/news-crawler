a1c0de1a3919ded70efd24ee9c5bdf0611510962
Nick Chapman <nchapman@gmail.com>
2020-06-28 14:48:18 -0700

    Fetching sources from news server.

--- news_crawler/spiders/site.py
+++ news_crawler/spiders/site.py
@@ -1,16 +1,80 @@
 import scrapy
 import uuid
 import datetime
-import yaml
+import urllib.request
+import json
+import re
 from scrapy_splash import SplashRequest
 from newspaper import Article
 from newspaper import urls
+
+EXTRA_ALLOWED_URLS = re.compile('(apnews.com/[a-z0-9])', re.IGNORECASE)
 
 class SiteSpider(scrapy.Spider):
     name = 'site'
 
     def start_requests(self):
-        lua_source = """
+        batch_id = uuid.uuid4().hex
+        batch_started_at = datetime.datetime.now()
+
+        for source in self.fetch_sources():
+            for section in source['sections']:
+                yield SplashRequest(
+                    url=section['url'],
+                    callback=self.parse,
+                    endpoint='execute',
+                    args={'lua_source': self.get_lua_source()},
+                    meta={
+                        'source_id': source['id'],
+                        'category': section['category'],
+                        'batch_id': batch_id,
+                        'batch_started_at': batch_started_at
+                    }
+                )
+
+    def parse(self, response):
+        for i, href in enumerate(response.css('a::attr(href)').extract()):
+            url = response.urljoin(href)
+            
+            if urls.valid_url(url) or EXTRA_ALLOWED_URLS.search(url):
+                yield scrapy.Request(url=url, callback=self.parse_story, meta={**response.meta, 'position': i})
+
+    def parse_story(self, response):
+        og_type = response.css('meta[property="og:type"]::attr(content)').extract_first()
+
+        if og_type == "article":
+            article = Article(url=response.url, language='en')
+            article.download(response.body)
+            article.parse()
+
+            amp_url = response.css('link[rel="amphtml"]::attr(href)').extract_first()
+            og_description = response.css('meta[property="og:description"]::attr(content)').extract_first()
+
+            yield {
+                'id': uuid.uuid4().hex,
+                'source_id': response.meta['source_id'],
+                'category': response.meta['category'],
+                'batch_id': response.meta['batch_id'],
+                'batch_started_at': response.meta['batch_started_at'],
+                'crawled_at': datetime.datetime.now(),
+                'position': response.meta['position'],
+                'title': article.title,
+                'authors': article.authors,
+                'description': og_description,
+                'text': article.text,
+                'image': article.top_image,
+                'publish_date': article.publish_date,
+                'url': response.url,
+                'canonical_url': article.canonical_link,
+                'amp_url': amp_url
+            }
+    
+    def fetch_sources(self):   
+        with urllib.request.urlopen("http://localhost:5500/api/sources") as url:
+            return json.loads(url.read().decode())
+    
+    def get_lua_source(self):
+        return """
             function main(splash)
                 local wait = 0.5
                 local num_scrolls = 5
@@ -33,61 +97,3 @@ class SiteSpider(scrapy.Spider):
                 return splash:html()
             end
         """
-
-        batch_id = uuid.uuid4().hex
-        batch_started_at = datetime.datetime.now()
-
-        with open('sites.yaml') as f:
-            sites = yaml.safe_load(f)
-
-        for site in sites:
-            for section_type, url in site['sections'].items():
-                yield SplashRequest(
-                    url=url, 
-                    callback=self.parse,
-                    endpoint='execute',
-                    args={'lua_source': lua_source},
-                    meta={
-                        'site_id': site['id'], 
-                        'section_type': section_type, 
-                        'batch_id': batch_id, 
-                        'batch_started_at': batch_started_at
-                    }
-                )
-
-    def parse(self, response):
-        for i, href in enumerate(response.css('a::attr(href)').extract()):
-            url = response.urljoin(href)
-            
-            if urls.valid_url(url):
-                yield scrapy.Request(url=url, callback=self.parse_story, meta={**response.meta, 'position': i})
-
-    def parse_story(self, response):
-        og_type = response.css('meta[property="og:type"]::attr(content)').extract_first()
-
-        if og_type == "article":
-            article = Article(url=response.url, language='en')
-            article.download(response.body)
-            article.parse()
-
-            amp_url = response.css('link[rel="amphtml"]::attr(href)').extract_first()
-            og_description = response.css('meta[property="og:description"]::attr(content)').extract_first()
-
-            yield {
-                'id': uuid.uuid4().hex,
-                'site_id': response.meta['site_id'],
-                'section_type': response.meta['section_type'],
-                'batch_id': response.meta['batch_id'],
-                'batch_started_at': response.meta['batch_started_at'],
-                'crawled_at': datetime.datetime.now(),
-                'position': response.meta['position'],
-                'title': article.title,
-                'authors': article.authors,
-                'description': og_description,
-                'text': article.text,
-                'image': article.top_image,
-                'publish_date': article.publish_date,
-                'url': response.url,
-                'canonical_url': article.canonical_link,
-                'amp_url': amp_url
-            }
